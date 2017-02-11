0878d32de027aa3aecd74d02d4f65e8678f1e049
Troll Station <govno1ebanoe@gmail.com>
2017-02-11 04:41:12 +0300

    some refactoring

--- main.py
+++ main.py
@@ -4,13 +4,20 @@
 
 import logging
 import requests
-from configparser import ConfigParser
+from time import sleep, time  # , gmtime, strftime
+from misc import stopwatch_countdown, line_print, init_config, init_logger, init_dbclient
 from sosach import Board, Thread, Post
-from time import sleep, time, gmtime, strftime
-from pymongo import MongoClient
-from misc import stopwatch_countdown, line_print
 
 __version__ = '0.1'
+
+# TODO: add argument passing support
+
+Config = init_config('config.conf')
+
+DBClient = init_dbclient(Config)
+DBLink = DBClient['2ch_parser_b']  # .format(config['database_prefix'])]
+ThLink = DBLink['threads']
+PLink = DBLink['posts']
 
 
 def get_board(name):
@@ -41,7 +48,7 @@ def get_board(name):
 
 
 def save_board(board):
-    logger.info('Saving {0} threads'.format(len(board.threads)))
+    logging.info('Saving {0} threads'.format(len(board.threads)))
     new_threads = 0
     new_posts = 0
     for thread in board.threads:
@@ -97,85 +104,44 @@ def save_board(board):
                 logging.debug(out_string + post_out_string)
                 line_print(out_string + post_out_string)
     line_print('')
-    logging.info('Saved {0} threads and {1} posts'.format(new_threads, new_posts))
-    logging.info('Stored {0} threads and {1} posts'.format(ThLink.count(), PLink.count()))
+    logging.info('Saved {0} new threads and {1} new posts'.format(new_threads, new_posts))
+    logging.info('Total stored {0} threads and {1} posts'.format(ThLink.count(), PLink.count()))
+
+
+def main():
+    init_logger(Config)
+    init_dbclient(Config)
+    wait_timeout = Config.getint('global', 'wait_timeout', fallback=600)
+    wait_timeout_fallback = Config.getint('global', 'wait_timeout_fallback', fallback=60)
+    while Config.getboolean('global', 'loop', fallback=True):
+        logging.info('=== START ===')
+        start_time = time()
+        start_fetch_time = time()
+        b_board = get_board('b')
+        logging.info(
+            'Fetched {0} threads in {1} seconds'.format(len(b_board.threads), int(time() - start_fetch_time)))
+        start_write_time = time()
+        save_board(b_board)
+        logging.info(
+            'Written {0} threads in {1} seconds'.format(len(b_board.threads), int(time() - start_write_time)))
+        total_time = int(time() - start_time)
+        logging.info('Total work time is {0} seconds'.format(total_time))
+        logging.info('=== STOP ===')
+        if total_time < wait_timeout:
+            stopwatch_countdown(wait_timeout - total_time,
+                                'Waiting update timeout {0} seconds.'
+                                .format(wait_timeout - total_time))
+        else:
+            logging.warning('Writing board time is longer than wait timeout')
+            stopwatch_countdown(wait_timeout_fallback,
+                                'Waiting update timeout {0} seconds.'.format(wait_timeout_fallback))
+        Config.read('config.conf')
 
 
 if __name__ == '__main__':
-    config = ConfigParser()
-    config.read('config.conf')
-    if 'global' not in config.sections():
-        config['global'] = {'wait_timeout': 1000,
-                            'wait_timeout_fallback': 60,
-                            'log_file': 'boardparser.log',
-                            'log_file_level': 'debug',
-                            'log_stdout_level': 'info',
-                            'mongodb_host': '127.0.0.1',
-                            'mongodb_port': 27017,
-                            'database_prefix': 'boardparser',
-                            'http_proxy': '',
-                            'https_proxy': ''}
-        with open('config.conf', 'w') as configfile:
-            config.write(configfile)
-    wait_timeout = int(config['global']['wait_timeout'])
-    wait_timeout_fallback = int(config['global']['wait_timeout_fallback'])
-    # TODO: move logging objects to misc
-    # log_filename = 'parser_{0}.log'.format(strftime('%d.%m.%y_%H:%M', gmtime()))
-    logging.basicConfig(format='%(asctime)s [%(levelname)s] : %(message)s', filename=config['global']['log_file'])
-    logging.addLevelName(logging.WARNING, '\033[1;31m{0}\033[1;0m'.format(logging.getLevelName(logging.WARNING)))
-    logging.addLevelName(logging.ERROR, '\033[1;41m{0}\033[1;0m'.format(logging.getLevelName(logging.ERROR)))
-    logger = logging.getLogger()
-    if config['global']['log_file_level'] == 'debug':
-        logger.setLevel(logging.DEBUG)
-    elif config['global']['log_file_level'] == 'info':
-        logger.setLevel(logging.INFO)
-    elif config['global']['log_file_level'] == 'warning':
-        logger.setLevel(logging.INFO)
-    elif config['global']['log_file_level'] == 'error':
-        logger.setLevel(logging.INFO)
-    else:
-        logging.error('Config reading failed : log_file_level option is invalid')
-    log_console_handler = logging.StreamHandler()
-    if config['global']['log_stdout_level'] == 'debug':
-        log_console_handler.setLevel(logging.DEBUG)
-    elif config['global']['log_stdout_level'] == 'info':
-        log_console_handler.setLevel(logging.INFO)
-    elif config['global']['log_stdout_level'] == 'warning':
-        log_console_handler.setLevel(logging.INFO)
-    elif config['global']['log_stdout_level'] == 'error':
-        log_console_handler.setLevel(logging.INFO)
-    else:
-        logging.error('Config reading failed : log_stdout_level option is invalid')
-    log_formatter = logging.Formatter('%(asctime)s [%(levelname)s] : %(message)s')
-    log_console_handler.setFormatter(log_formatter)
-    logger.addHandler(log_console_handler)
-    DBClient = MongoClient(config['global']['mongodb_host'], int(config['global']['mongodb_port']))
-    DBLink = DBClient['2ch_parser_b']  # .format(config['database_prefix'])]
-    ThLink = DBLink['threads']
-    PLink = DBLink['posts']
+    print(chr(27) + "[2J")  # Clear terminal
     try:
-        while True:
-            logging.info('=== START ===')
-            start_time = time()
-            start_fetch_time = time()
-            b_board = get_board('b')
-            logging.info(
-                'Fetched {0} threads in {1} seconds'.format(len(b_board.threads), int(time() - start_fetch_time)))
-            start_write_time = time()
-            save_board(b_board)
-            logging.info(
-                'Written {0} threads in {1} seconds'.format(len(b_board.threads), int(time() - start_write_time)))
-            total_time = int(time() - start_time)
-            logging.info('Total work time is {0} seconds'.format(total_time))
-            logging.info('=== STOP ===')
-            if total_time < wait_timeout:
-                stopwatch_countdown(wait_timeout - total_time,
-                                    'Waiting update timeout {0} seconds.'
-                                    .format(wait_timeout - total_time))
-            else:
-                logging.warning('Writing board time is longer than wait timeout')
-                stopwatch_countdown(wait_timeout_fallback,
-                                    'Waiting update timeout {0} seconds.'.format(wait_timeout_fallback))
+        main()
     except KeyboardInterrupt:
         line_print('=== User exit ===')
         exit()
--- misc.py
+++ misc.py
@@ -1,8 +1,68 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import logging
+from configparser import ConfigParser
 from time import time, clock, sleep
 from shutil import get_terminal_size
+from pymongo import MongoClient
+
+
+def init_config(file):
+    config = ConfigParser()
+    config.read(file)
+    if 'global' not in config.sections():
+        config['global'] = {'loop': 1,
+                            'wait_timeout': 1000,
+                            'wait_timeout_fallback': 60,
+                            'log_file': 'boardparser.log',
+                            'log_file_level': 'debug',
+                            'log_stdout_level': 'info',
+                            'mongodb_host': '127.0.0.1',
+                            'mongodb_port': 27017,
+                            'database_prefix': 'boardparser',
+                            'http_proxy': '',
+                            'https_proxy': ''}
+        with open('config.conf', 'w') as configfile:
+            config.write(configfile)
+    return config
+
+
+def init_logger(config):
+    # log_filename = 'parser_{0}.log'.format(strftime('%d.%m.%y_%H:%M', gmtime()))
+    logging.basicConfig(format='%(asctime)s [%(levelname)s] : %(message)s', filename=config['global']['log_file'])
+    logging.addLevelName(logging.WARNING, '\033[1;31m{0}\033[1;0m'.format(logging.getLevelName(logging.WARNING)))
+    logging.addLevelName(logging.ERROR, '\033[1;41m{0}\033[1;0m'.format(logging.getLevelName(logging.ERROR)))
+    logger = logging.getLogger()
+    if config['global']['log_file_level'] == 'debug':
+        logger.setLevel(logging.DEBUG)
+    elif config['global']['log_file_level'] == 'info':
+        logger.setLevel(logging.INFO)
+    elif config['global']['log_file_level'] == 'warning':
+        logger.setLevel(logging.INFO)
+    elif config['global']['log_file_level'] == 'error':
+        logger.setLevel(logging.INFO)
+    else:
+        logging.error('Config reading failed : log_file_level option is invalid')
+    log_console_handler = logging.StreamHandler()
+    if config['global']['log_stdout_level'] == 'debug':
+        log_console_handler.setLevel(logging.DEBUG)
+    elif config['global']['log_stdout_level'] == 'info':
+        log_console_handler.setLevel(logging.INFO)
+    elif config['global']['log_stdout_level'] == 'warning':
+        log_console_handler.setLevel(logging.INFO)
+    elif config['global']['log_stdout_level'] == 'error':
+        log_console_handler.setLevel(logging.INFO)
+    else:
+        logging.error('Config reading failed : log_stdout_level option is invalid')
+    log_formatter = logging.Formatter('%(asctime)s [%(levelname)s] : %(message)s')
+    log_console_handler.setFormatter(log_formatter)
+    logger.addHandler(log_console_handler)
+
+
+def init_dbclient(config):
+    return MongoClient(config.get('global', 'mongodb_host', fallback='127.0.0.1'),
+                       config.getint('global', 'mongodb_port', fallback=27017))
 
 
 # le костыль
